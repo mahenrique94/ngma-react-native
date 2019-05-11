421302dc726735ea3ee12c157bdeb08bf55ed268
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2019-05-11 09:05:13 -0300

    config: Init setup of the multi app architecture with lerna

---
+++ _core/src/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Text, View } from 'react-native'
+
+const App = () => (
+    <View>
+        <Text>{'app.name'}</Text>
+    </View>
+)
+
+export default App
---
+++ herbalife/index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native'
+
+import App from '../_core/containers/App'
+
+import { name as appName } from './app.json'
+
+AppRegistry.registerComponent(appName, () => App)
