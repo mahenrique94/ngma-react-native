27943bde3cef97f772b34087063341e6f5f952ff
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2019-05-11 09:29:57 -0300

    config: First simple POC config

--- herbalife/index.js
+++ herbalife/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native'
 
-import App from '../_core/containers/App'
+import App from '@ngma/core/containers/App'
 
 import { name as appName } from './app.json'
 
