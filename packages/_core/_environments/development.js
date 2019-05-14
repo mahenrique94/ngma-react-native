ad4e22dfca46f2d897078aeddda5ff2a32c00516
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2019-05-14 15:45:18 -0300

    feature: First basic version working

---
+++ packages/_core/_environments/development.js
@@ -0,0 +1,8 @@
+import { configs } from '../constants/environments'
+
+const config = new Map()
+config.set(configs.NGMA_APP_PRIMARY_COLOR, 'white')
+config.set(configs.NGMA_APP_TITLE, 'NGMA')
+config.set(configs.NGMA_APP_TITLE_COLOR, 'red')
+
+export { config }
